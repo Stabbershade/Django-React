ab954a951e687524ef7ec51c3da9e1319dfbfc1f
Stabbershade <Stabbershade@hotmail.com>
2023-09-06 23:27:51 +0800

    <feat>(UI) Implemented routing for signin and signup pages

--- django_project/frontend/src/App.tsx
+++ django_project/frontend/src/App.tsx
@@ -2,12 +2,15 @@ import React from "react"
 import { BrowserRouter as Router, Routes , Route , Link , redirect} from "react-router-dom"
 import { Sample1 } from "./components/Sample1"
 import { Sample2 } from "./components/Sample2"
+import  SignIn  from "./components/SignIn"
+import SignUp  from "./components/SignUp"
 
 export const App = () =>{
   return (
     <Router>
       <Routes>
-        <Route path="frontend/" element={<h1>This is the main Page</h1>}></Route>
+        <Route path="frontend/" element={<SignIn />}></Route>
+        <Route path="frontend/Signup" element={<SignUp />}></Route>
         <Route path="frontend/Sample1" element={<Sample1 />}></Route>
         <Route path="frontend/Sample2" element={<Sample2 />}></Route>
       </Routes>
--- django_project/frontend/urls.py
+++ django_project/frontend/urls.py
@@ -3,6 +3,7 @@ from .views import index
 
 urlpatterns = [
     path('', index),
+     path('Signup/', index),
     path('Sample1/', index),
     path('Sample2/', index),
 ]
